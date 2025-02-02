767846003111e5b3bb513a7961e407b5c1f5701c
LFMG1993 <lfmontes662@soy.sena.edu.co>
2025-02-01 22:56:33 -0500

    primer commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAe8aZ1KmgeWZ0SV5X_SBPnjU_2s6ia-rw",
+    authDomain: "congelados-21989.firebaseapp.com",
+    projectId: "congelados-21989",
+    storageBucket: "congelados-21989.firebasestorage.app",
+    messagingSenderId: "1027003013526",
+    appId: "1:1027003013526:web:2243dabf0cf6827a07a5b7",
+    measurementId: "G-EVWSP8LGVM"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
